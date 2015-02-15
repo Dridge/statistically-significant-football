db886ddd04dd3634f3b3c2a3c2cc069105ce1208
Richard Eldridge <richard.eldridge2010@googlemail.com>
2015-02-15 23:54:35 +0000

    Added FileManager tester

---
+++ src/test/FileManagerTester.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.github.dridge.files.FileManager;
+import org.junit.Test;
+
+import java.io.File;
+
+import static junit.framework.TestCase.fail;
+
+/**
+ * Created by Richard on 15/02/2015.
+ */
+public class FileManagerTester {
+
+    @Test
+    public void testCreateFile() {
+        FileManager fm = new FileManager();
+        fm.createFile();
+        File f = new File(fm.getFileLocation());
+        if (!f.exists()){
+            fail();
+        }
+    }
+}
